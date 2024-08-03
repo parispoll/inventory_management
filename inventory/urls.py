bd0a132f2b29a0fbffa24903bdd69f51df6e2fcb
Paris <paris_pollezel@hotmail.com>
2024-08-03 20:41:43 +0300

    Edit item redirects correctly

--- inventory/urls.py
+++ inventory/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', Index.as_view(), name='index'),
     path('dashboard/', Dashboard.as_view(), name='dashboard'),
     path('add-item/', AddItem.as_view(), name='add-item'),
-    path('edit-item/<int:pk>', EditItem.as_view(), name='edit-item'),
+    path('edit-item/<int:item_id>/', EditItem.as_view(), name='edit-item'),
     path('delete-item/<int:pk>', DeleteItem.as_view(), name='delete-item'),
     path('signup/', SignUpView.as_view(), name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='inventory/login.html'), name='login'),
